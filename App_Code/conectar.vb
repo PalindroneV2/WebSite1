6e32ef5f201c1da8fb0b21ce40ad575f11b57f3b
PalindroneV2 <asfadasfag@gmail.com>
2022-03-17 00:45:03 -0700

    Update conectar.vb

--- App_Code/conectar.vb
+++ App_Code/conectar.vb
@@ -6,7 +6,7 @@ Imports System.Data
 Public Class conectar
     Public Shared Function conec_string() As String
         Dim strConnString As String = ""
-        strConnString = "Server=127.0.0.1;User id=root;Password=123456;Database=empresa_servicios;pooling=false;port=3306;default command timeout=0;CharSet=utf8;"
+        strConnString = "Server=127.0.0.1;User id=root;Password=rootpass;Database=empresa_servicios;pooling=false;port=3306;default command timeout=0;CharSet=utf8;"
         Return strConnString
     End Function
 End Class
